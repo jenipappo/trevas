02181cc78734a4d77a9f6525f3da1111dcb46a06
brunovinicius <bruno.vinicius.ds@gmail.com>
2018-06-04 01:10:32 -0300

    - fix weapon name being on WeaponMastery instead of Weapon - add weapon kind to WeaponMastery so we can relate them

--- lib/model/WeaponMastery.dart
+++ lib/model/WeaponMastery.dart
@@ -1,14 +1,15 @@
 import 'package:trevas/model/Character.dart';
+import 'package:trevas/model/WeaponKind.dart';
 
 
 class WeaponMastery {
   final Attributes _attributes;
 
-  final String name;
   final int baseAttack;
   final int baseDefense;
+  final WeaponKind kind;
 
-  WeaponMastery(this.name, this.baseAttack, this.baseDefense, this._attributes);
+  WeaponMastery(this.kind, this.baseAttack, this.baseDefense, this._attributes);
 
   get attack => _attributes.dexterity.current + baseAttack;
   get defense => _attributes.dexterity.current + baseDefense;
